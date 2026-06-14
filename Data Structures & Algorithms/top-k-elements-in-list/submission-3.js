e73a8c73d7c6bfe3887301570ad9878512e01a9a
Bryan <112831220+yuyangchen23@users.noreply.github.com>
2026-06-14 23:16:09 +0800

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+
+    topKFrequent(nums, k) {
+        let hashTable = new Map();
+
+        for (const n of nums) {
+            if (!hashTable.has(n)) {
+                hashTable.set(n, 1);
+            } else {
+                const currentValue = hashTable.get(n) || 0;
+                hashTable.set(n, currentValue + 1);
+            }
+        }
+
+        // let buckets = Array.from({length: nums.length + 1}, () => []);
+        // for (const [num, freq] of hashTable.entries()) {
+        //     buckets[freq].push(num);
+        // }
+
+        // let res = [];
+        // for (let i = buckets.length - 1; i >= 0 && res.length < k; i--) {
+        //     if (buckets[i].length > 0) {
+        //         res.push(...buckets[i]);
+        //     }
+        // }
+
+        const sortMap = [...hashTable.entries()]
+                        .sort((a, b) => b[1] - a[1])
+                        .slice(0, k)
+                        .map(([key, val]) => key);
+
+        return sortMap;
+
+        // return res.slice(0, k);
+    }
+}
