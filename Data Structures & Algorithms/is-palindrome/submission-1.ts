0aceac3ab33af7f0668151052faa86ca9a8728f1
Bryan <112831220+yuyangchen23@users.noreply.github.com>
2026-07-04 20:17:18 +0800

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        
+        let onlyAlpha = (s.replace(/[^a-zA-Z0-9]/g, "")).toLowerCase();
+
+        let left = 0;
+        let right = onlyAlpha.length - 1;
+        
+        while (left < right) {
+            if (onlyAlpha[left] !== onlyAlpha[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
