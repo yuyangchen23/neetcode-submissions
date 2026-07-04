e6237c84acc35d7cff37e9fe2b1469e7fe7d06b1
Bryan <112831220+yuyangchen23@users.noreply.github.com>
2026-07-04 17:47:24 +0800

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        const hashSet = new Set<number>(nums);
+        let longestStreak = 0;
+
+        for (const num of hashSet) {
+            // [20, 10, 11, 12, 13]
+            if (!hashSet.has(num - 1)) {
+                let currentNum = num;
+                let currentStreak = 1;
+
+                while (hashSet.has(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                if (currentStreak > longestStreak) {
+                    longestStreak = currentStreak;
+                }
+            }
+        }
+
+        return longestStreak;
+    }
+}
