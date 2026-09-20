d88d833ddb604eb40313ffe5c74bd46004593a7e
Bryan <112831220+yuyangchen23@users.noreply.github.com>
2026-09-20 12:57:45 +0800

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones: number[]): number {
+        let maxHeap: any = new MaxPriorityQueue();
+
+        for (const s of stones) {
+            maxHeap.enqueue(s);
+        }
+
+        while (maxHeap.size() > 1) {
+            maxHeap.enqueue(maxHeap.pop() - maxHeap.pop());
+        }
+
+
+        return maxHeap.pop();
+
+    }
+}
